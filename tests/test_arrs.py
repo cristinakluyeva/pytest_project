4b3a72bb3e154c45c0221f98d824f83da4cbf70b
Cristina <kristikavai@mail.ru>
2023-08-11 19:10:54 +0300

    add pytest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,7 +6,7 @@ from utils import arrs
     (["ok", "good", "not_bad"], 1, "lose", "good"),
     ([3, 6, 8, 9, 0], 3, "lose", 9),
     ([3, 6, 8, 9, 0], -2, "lose", "lose"),
-    ([0.9, 0,5, "hello"], -2, "lose", "lose")
+    ([0.9, 0.5, "hello"], -2, "lose", "lose")
 ])
 
 
@@ -19,8 +19,9 @@ def test_get():
 
 
 def test_slice():
+    assert arrs.my_slice([], 1, 3) == []
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([0, 0, 0], -1) == [0]
-    assert arrs.my_slice(["a", "b", "c", "d"], -4, -2) == ["a", "b"]
+    assert arrs.my_slice(["a", "b", "c", "d"], -5, -2) == ["a", "b"]
     assert arrs.my_slice(["a", "b", "c", "d"], 0, -2) == ["a", "b"]
